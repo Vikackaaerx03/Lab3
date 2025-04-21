b32e8a92e022255949f84c9604727a4db3aa6b59
Vikackaaerx03 <vikaroslav00@gmail.com>
2025-04-21 18:33:53 +0300

    script.js

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ function handleScroll() {
             meme.classList.add('visible');
         }
     });
+
+    // 🔽 Додаємо анімацію до списку
+    const listItems = document.querySelectorAll('ul li');
+    listItems.forEach((item, index) => {
+        if (isInViewport(item)) {
+            setTimeout(() => {
+                item.classList.add('visible');
+            }, index * 150); // затримка 150 мс для кожного
+        }
+    });
 }
 
 window.addEventListener('scroll', handleScroll);
